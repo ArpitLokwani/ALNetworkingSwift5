3076c29f8334f810855e6e92d00b0c8679653c77
bizofitmobile <info@bizofit.com>
2020-04-29 11:09:40 +0530

    Initial Commit

---
+++ ALNetworking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ALNetworking
+//
+//  Created by Arpit Lokwani on 29/04/20.
+//  Copyright © 2020 Arpit Lokwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
